b6eae7b3170f621ee744be899963f1490245477b
Max <maximgusho7@gmail.com>
2023-09-26 20:04:38 +0600

    ADD: Update

--- conftest.py
+++ conftest.py
@@ -9,10 +9,10 @@ def driver():
     options = webdriver.ChromeOptions()
     options.page_load_strategy = 'eager' # for not waiting the web page fully downloads
     # options.headless = True
-    # options.add_argument('--disable-gpu')
+    options.add_argument('--disable-gpu')
     options.add_argument('--headless')
-    # options.add_argument('--no-sandbox')
-    # options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=options)
 
     driver.get('https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all')
